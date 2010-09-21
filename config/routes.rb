c88268143f6b5cf17a88c8f7670908b709da1f3a
Leo Valk <leo@leovalk.com>
2010-09-21 16:09:10 -0500

    allemaal styling gedaan

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+ # get "pages/home"
 
-  get "pages/contact"
+  #get "pages/contact"
   
-  get "pages/about"
+ # get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -61,4 +61,9 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  SampleApp::Application.routes.draw do
+match '/contact', :to => 'pages#contact'
+match '/about', :to => 'pages#about'
+match '/help', :to => 'pages#help'
+  root :to => 'pages#home'
 end
